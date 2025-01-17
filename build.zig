70bda706587b1dbd3e55efecef1b8592b24f3c4f
march <mar@march>
2025-01-17 11:52:02 +0100

    generate c++ file

--- build.zig
+++ build.zig
@@ -1,21 +1,33 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const vma_dep = b.dependency("vma", .{});
-    const vma_include_path = vma_dep.path("include");
-    const vma = b.addStaticLibrary(.{
+    const dep = b.dependency("vma", .{});
+    const include_path = dep.path("include");
+    const lib = b.addStaticLibrary(.{
         .name = "vma",
         .target = target,
         .optimize = optimize,
         .link_libc = true,
     });
-    vma.linkLibCpp();
+    lib.linkLibCpp();
 
-    vma.addCSourceFile(.{ .file = b.path("src/vma.cc"), .flags = &.{"-std=c++17"} });
-    vma.addIncludePath(vma_include_path);
+    // Generate implementation
+    const impl = try std.fs.createFileAbsolute(
+        try dep.path("vma.cc").getPath3(b, &lib.step).toString(b.allocator),
+        .{},
+    );
+    try impl.writeAll(
+        \\#define VMA_IMPLEMENTATION
+        \\#define VMA_STATIC_VULKAN_FUNCTIONS 0
+        \\#include <vk_mem_alloc.h>
+    );
+    impl.close();
+
+    lib.addCSourceFile(.{ .file = dep.path("vma.cc"), .flags = &.{"-std=c++17"} });
+    lib.addIncludePath(include_path);
 
     var module = b.addModule("vk-mem-alloc-zig", .{
         .target = target,
@@ -23,5 +35,5 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
     });
 
-    module.linkLibrary(vma);
+    module.linkLibrary(lib);
 }
